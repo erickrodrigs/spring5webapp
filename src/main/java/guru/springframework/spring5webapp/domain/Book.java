25fcd75c155adbc717bc0c0f71f30fd4cba18d75
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-02-02 17:01:13 -0300

    add Publisher entity
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -13,6 +13,9 @@ public class Book {
     private String title;
     private String isbn;
 
+    @ManyToOne
+    private Publisher publisher;
+
     @ManyToMany
     @JoinTable(
             name = "author_book",
@@ -51,6 +54,14 @@ public class Book {
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     public Set<Author> getAuthors() {
